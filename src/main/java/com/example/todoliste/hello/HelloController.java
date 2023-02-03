5713cbb9042d66081a2258b0817ffd29d63efe36
Beşir AYDEMİR <javahesaphesap@gmail.com>
2023-02-03 23:53:33 +0300

    ilkhali

---
+++ src/main/java/com/example/todoliste/hello/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.todoliste.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/hello")
+	public String sayHello() {
+		
+		
+		return "beşir";
+		
+	}
+
+}
---
+++ src/test/java/com/example/todoliste/TodolisteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todoliste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolisteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
